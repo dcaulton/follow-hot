ae0ac8ecfebd7aef25498adb983739a93a854c3a
Dave Caulton <dcaulton@gmail.com>
2017-02-13 18:15:48 -0600

    Reconcile that getting and setting azimuth uses different coordinate systems.
    Settle on 180 = straight, degrees increment clockwise

--- follow_hot/platform/platform.py
+++ follow_hot/platform/platform.py
@@ -24,10 +24,23 @@ class Platform(object):
             status_dict = {}
             status_dict.update([x.split('=', 1) for x in response.content.split()])
             azimuth = float(status_dict['status.Postion[0]']) # YES, Amcrest misspelled Position in their API 
+            azimuth = self._convert_to_180_center(azimuth)
             zenith = float(status_dict['status.Postion[1]'])
-            # todo: convert from [180-360,0-180],[0-100] to [0-360], [0-100]
             return (azimuth, zenith)
         raise Exception('bad return code from getting platform position: {0}'.format(response.status_code))
+
+    def _convert_to_180_center(self, azimuth):
+        # converts azimuth from the coordinate system returned by get_position, to the kind that's used by goto_position
+        # incoming: degrees counterclockwise from front=360/0
+        # desired: degrees clockwise from back=0
+        if 0 <= azimuth <= 180:
+            #its counterclockwise of straight ahead
+            azimuth = 180 - azimuth
+        else:
+            #its to the left of straight ahead
+            azimuth = 180 + (360 - azimuth)
+        return azimuth
+
     def goto_position(self, azimuth, zenith):
         the_url = 'http://{0}:{1}@{2}/cgi-bin/ptz.cgi?action=start&channel=0&code=PositionABS'\
             '&arg1={3}&%arg2={4}&arg3=0'.format(
